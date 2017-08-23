a08281d1d4375c6437a4221e2ae3f3055a043cae
Анна <@LAPTOP-63K8I5TL>
2017-08-24 00:10:58 +0500

    Решение

--- Task 9/Program.cs
+++ Task 9/Program.cs
@@ -8,79 +8,75 @@ namespace Task_9
 {
     class Program
     {
-
-
-
-        static void Main(string[] args)
+        /// <summary>
+        /// Проверка ввода целых чисел
+        /// </summary>
+        /// <returns>Целое число</returns>
+        static int CheakReadInt()
         {
-            Console.WriteLine("Введите количество элементов в списке");
-            int N = 0;
-            bool ok;            
+            int k = 0; bool ok;
             do
             {
-                ok = int.TryParse(Console.ReadLine(), out N);
+                ok = int.TryParse(Console.ReadLine(), out k);
                 if (!ok) Console.WriteLine("Неправильный ввод. Ожидалось натуральное число. Пожалуйста, повторите ввод");
             }
             while (!ok);
 
-            DoublyList er = DoublyList.MakeDoublyList(N);
-            DoublyList.ShowDoublyList(er);
+            return k;
+        }
 
-            // Поиск элементов
-            // По номеру
-            Console.WriteLine("Введите номер элемента для поиска");
-            int num = 0;
-            do
-            {
-                ok = int.TryParse(Console.ReadLine(), out num);
-                if (!ok) Console.WriteLine("Неправильный ввод. Ожидалось натуральное число. Пожалуйста, повторите ввод");
-            }
-            while (!ok);
-
-            DoublyList foundEl = DoublyList.SearchElemNum(er, num);
-            Console.WriteLine(foundEl);
-
-            // По данным
-            Console.WriteLine("Введите данные элемента для поиска");
-            double data = 0;
+        /// <summary>
+        /// Проверка ввода вещественных чисел
+        /// </summary>
+        /// <returns>Вещественное число</returns>
+        static double CheakReadDouble()
+        {
+            double data = 0; bool ok;
             do
             {
                 ok = double.TryParse(Console.ReadLine(), out data);
                 if (!ok) Console.WriteLine("Неправильный ввод. Ожидалось вещественное число. Пожалуйста, повторите ввод");
             }
             while (!ok);
+            return data;
+        }
 
-            foundEl = DoublyList.SearchElemData(er, data);
+
+        static void Main(string[] args)
+        {
+            // Создание списка
+            Console.WriteLine("Введите количество элементов в списке");
+            int N = CheakReadInt();
+            DoublyList list = DoublyList.MakeDoublyList(N);
+            DoublyList.ShowDoublyList(list);
+
+
+            // Поиск элементов
+            // По номеру
+            Console.WriteLine("Введите номер элемента для поиска");
+            int num = CheakReadInt();
+            DoublyList foundEl = DoublyList.SearchElemNum(list, num);
+            Console.WriteLine(foundEl);
+
+            // По данным
+            Console.WriteLine("Введите данные элемента для поиска");
+            double data = CheakReadDouble();            
+            foundEl = DoublyList.SearchElemData(list, data);
             Console.WriteLine(foundEl);
 
 
             // Удаление элементов
             // По номеру
             Console.WriteLine("Введите номер элемента для удаления");
-            num = 0;
-            do
-            {
-                ok = int.TryParse(Console.ReadLine(), out num);
-                if (!ok) Console.WriteLine("Неправильный ввод. Ожидалось натуральное число. Пожалуйста, повторите ввод");
-            }
-            while (!ok);
-
-            er = DoublyList.DeleteElemNum(er, num);
-            DoublyList.ShowDoublyList(er);
+            num = CheakReadInt();
+            list = DoublyList.DeleteElemNum(list, num);
+            DoublyList.ShowDoublyList(list);
 
             // По данным
             Console.WriteLine("Введите данные элемента для удаления");
-            data = 0;
-            do
-            {
-                ok = double.TryParse(Console.ReadLine(), out data);
-                if (!ok) Console.WriteLine("Неправильный ввод. Ожидалось вещественное число. Пожалуйста, повторите ввод");
-            }
-            while (!ok);
-
-            er = DoublyList.DeleteElemData(er, data);
-            DoublyList.ShowDoublyList(er);
-
+            data = CheakReadDouble();
+            list = DoublyList.DeleteElemData(list, data);
+            DoublyList.ShowDoublyList(list);
 
             Console.ReadLine();
         }
